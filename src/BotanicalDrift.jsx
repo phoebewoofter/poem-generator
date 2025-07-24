edc23858f7101bbf7552e59f0db20f7584e893bf
pwoofter <phoebewoofter@gmail.com>
2025-07-24 14:10:44 -0700

    Backtracks botanical drift

--- src/BotanicalDrift.jsx
+++ src/BotanicalDrift.jsx
@@ -4,13 +4,13 @@ function BotanicalDrift() {
 
     return (
         <>
-            <div class="falling-area">
-                <div class="petal falling" style="--i:0;"></div>
-                <div class="petal falling" style="--i:1;"></div>
-                <div class="petal falling" style="--i:2;"></div>
+            <div className="falling-area">
+                <div className="petal falling" style="--i:0;"></div>
+                <div className="petal falling" style="--i:1;"></div>
+                <div className="petal falling" style="--i:2;"></div>
             </div>
 
-            <div class="accumulated-pile"></div>
+            <div className="accumulated-pile"></div>
         </>
     )
 }
