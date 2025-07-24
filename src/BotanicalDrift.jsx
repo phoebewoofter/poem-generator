b7dbd5619636f16817ccaddea94baf2b76bb9905
pwoofter <phoebewoofter@gmail.com>
2025-07-24 13:31:18 -0700

    Creates html structure for botanical drift effect

--- src/BotanicalDrift.jsx
+++ src/BotanicalDrift.jsx
@@ -1,1 +1,18 @@
 import './BotanicalDrift.css'
+
+function BotanicalDrift() {
+
+    return (
+        <>
+            <div class="falling-area">
+                <div class="petal falling" style="--i:0;"></div>
+                <div class="petal falling" style="--i:1;"></div>
+                <div class="petal falling" style="--i:2;"></div>
+            </div>
+
+            <div class="accumulated-pile"></div>
+        </>
+    )
+}
+
+export default BotanicalDrift
